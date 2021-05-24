9cef822d2368bb8631bbb077582d82cac55f4a6d
Kracha0907 <73388987+Kracha0907@users.noreply.github.com>
2021-05-24 03:44:43 +0300

    Update menu.php

--- simple_votings/menu.php
+++ simple_votings/menu.php
@@ -1,20 +1,56 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-<link href="styles/style.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <style type="text/css">
+        ul {
+            list-style: none; /*убираем маркеры списка*/
+            margin: 0; /*убираем отступы*/
+            padding-left: 0; /*убираем отступы*/
+            margin-top:25px; /*делаем отступ сверху*/
+        }
+        a {
+            text-decoration: none; /*убираем подчеркивание текста ссылок*/
+            background:#30A8E6; /*добавляем фон к пункту меню*/
+            color:#fff; /*меняем цвет ссылок*/
+            padding:10px; /*добавляем отступ*/
+            font-family: arial; /*меняем шрифт*/
+            border-radius:4px; /*добавляем скругление*/
+            -moz-transition: all 0.3s 0.01s ease; /*делаем плавный переход*/
+            -o-transition: all 0.3s 0.01s ease;
+            -webkit-transition: all 0.3s 0.01s ease;
+        }
+        a:hover {
+            background:#1C85BB;/*добавляем эффект при наведении*/
+        }
+        li {
+            float:left; /*Размещаем список горизонтально для реализации меню*/
+            margin-right:5px; /*Добавляем отступ у пунктов меню*/
+        }
+    </style>
+</head>
+
 <div>
 <ul>
 <li><a href="index.php">Главная</a></li>
-<li><a href="check-in.php">Регистрация</a></li>
+<li><a href="tests.php">Тесты</a></li>
+<li><a href="check-in_form.php">Регистрация</a></li>
 <li><a href="auth.php">Войти</a></li>
 <li><a href="contacts.php">Контакты</a></li>
+<li><a href="profil.php">Профиль</a></li>
+<li><a href="logout.php">Выйти</a></li>
 </ul>
 </div>
-
-</head>
-
-<body>
-
-</body>
+<br>
+<br>
+<br>
 
 </html>
