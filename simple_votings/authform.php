46be7108ce5ebab61d550f39874d3bedd0630f2c
Kracha0907 <73388987+Kracha0907@users.noreply.github.com>
2021-05-24 03:42:40 +0300

    Update authform.php

--- simple_votings/authform.php
+++ simple_votings/authform.php
@@ -9,8 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Форма авторизации</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link href="styles/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
   <br>
